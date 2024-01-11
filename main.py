01ae5471114252501c6d7f7ed37a877ccf82b447
lfb-julien <155462070+lfb-julien@users.noreply.github.com>
2024-01-11 10:01:53 +0100

    Update main.py
    rm comment

--- main.py
+++ main.py
@@ -18,64 +18,6 @@ async def revenu_fiscal_moyen(year: int = Query(...), city: str = Query(...)):
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
-"""
-Explication du code point par point
-
-@app.get("/revenu_fiscal_moyen/", description="1. Obtenir le revenu fiscal moyen des foyers d'une ville pour une année donnée.")
- Dans le code FastAPI, @app.get est utilisé pour définir un endpoint dans une application FastAPI.
- On indique ensuite l'url cible(relatif) et une description
-
- async def revenu_fiscal_moyen(year: int = Query(...), city: str = Query(...)):
- pour rappel def permet de dire qu'on va crée une fonction. Pour async: Il est utilisé pour définir des fonctions asynchrones. 
-Les fonctions asynchrones, également appelées coroutines, permettent l'exécution de plusieurs tâches de manière 
-concurrente sans bloquer le programme principal. En gros par exemple une tache qu'on veut exacuté sois même apres execution du programme
-via un click sur un bouton
-
-
-  try:
-        with sqlite3.connect(r"chinook.db") as con:
-            cur = con.cursor()
-            res = cur.execute(f"SELECT revenu_fiscal_moyen FROM foyers_fiscaux WHERE date = {year} AND ville = '{city}'")
-            result = res.fetchone()
-
-On crée une condition pour testé si on trouve la donnée via notre requette.
-on dit avec quoi on se connecte et a quoi
-on crée le curseur 
-et on l'utilise pour faire la requette avec les ptite variable précédement faite
-on met le resultat dans result
-
-petite explication pour fetchone() ou all()
-fetchone() renvoie un seul tuple représentant la première ligne de résultats.
-fetchall() renvoie une liste de tuples représentant toutes les lignes de résultats.
-Si la requête ne renvoie aucun résultat, fetchone() renvoie None, et fetchall() renvoie une liste vide ([]).
-L'utilisation de fetchall() peut être inefficace pour de grandes quantités de données car elle récupère toutes les lignes en mémoire.
-En général, pour des résultats potentiels importants, il est préférable d'utiliser fetchone() et de parcourir les résultats au fur et à mesure 
-pour éviter une utilisation excessive de la mémoire. 
-
-Pour faire plus simple c'est pour dire si on veut une liste ou pas....
-
-
- if result is None:
-                raise HTTPException(status_code=404, detail="Pas de résultat trouvé")
-si il y a pas de resultat car pas de donnée trouvé on affiche pas de résultat trouvé
-
-else:
-                return {"revenu_fiscal_moyen": result[0]}
-et si non on affiche le resultat
-
-petite précision:
-
-si la requête SQL renvoie plusieurs colonnes, result[0], result[1], etc., font référence aux valeurs de chaque colonne 
-pour la première ligne de résultats.
-Si il n'y a qu'une colone on met 0
-
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
-Dans le contexte du code FastAPI, le bloc except Exception as e: est utilisé pour capturer toute exception
- qui pourrait se produire lors de l'exécution du code à l'intérieur du bloc try. Ensuite, il utilise FastAPI 
- pour lever une exception HTTP (HTTPException) avec un code d'état 500 (Erreur interne du serveur) 
- et le détail de l'exception en tant que message.
-"""
 # 2. En tant qu'Agent je veux consulter les 10 dernières transactions dans ma ville (Lyon)
 @app.get("/top10_transaction/", description="2. Obtenir les 10 dernières transactions dans une ville.")
 async def top_transaction_10(city: str = Query(...)):
